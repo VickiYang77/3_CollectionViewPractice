a2de6991599e999a777ec3771e1c1982d506e286
Vicki <ymasakot@gmail.com>
2022-09-22 23:17:46 +0800

    init

---
+++ 3_CollectionViewPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3_CollectionViewPractice
+//
+//  Created by Vicki Yang on 2022/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
